25d3fc32d6aa9b8654c9dfb31281f5fec710d934
Beema Rahim <beemarahim21@gmail.com>
2021-06-10 22:11:43 +0530

    adding javascript programs

---
+++ javascriptprograms/fundamentals.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+var age ="25";
+var p_name="beema"
+console.log("hai am"+ p_name,age+"years old")
+//hi am beema 25 years old
+var state="kerala"
+var active_cases=15000;
+var cured=20000;
+var death=200;
+//in kerala 15000 new cases and 20000 cured 200 death has been reported 
+console.log(`in ${state}${active_cases}new cases and ${cured} active cases${death}death has been reported`);
+var c_name="link"
+console.log(typeof(c_name));
---
+++ javascriptprograms/swapping.js
@@ -0,0 +1,14 @@
+var num1=10,num2=20;
+
+console.log(`varles b4 swapping num1=${num1} num2=${num2}`);
+
+//logic for swapping two numbers
+//var temp=num1;
+//num1=num2;
+//num2=temp;
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+
+
+console.log(`varles after swapping num1=${num1} num2=${num2}`);
